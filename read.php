822c1c38cf0836c96392ef35fb20b4d6c428e8cd
samarthhalyal@gmail.com <samarthhalyal@gmail.com>
2019-03-30 21:38:32 +0530

    small update

--- read.php
+++ read.php
@@ -1,10 +1,6 @@
 <?php 
 
 $output = shell_exec('php test1.php');
-<<<<<<< HEAD
-header('Location: home.php');
-=======
 header('Location: home.html');
->>>>>>> f30dd9931e857a6b5be04d6d6c1fdd560ac3b342
 
 ?>
